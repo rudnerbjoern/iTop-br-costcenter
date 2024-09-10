9127834de41a2ea5a7c2dafddccd60bd826280fc
Bjoern Rudner <rudnerbjoern@hotmail.com>
2024-09-10 16:25:23 +0200

    set version 2.7.4

--- br-costcenter-bridge-for-br-medicaldevice/module.br-costcenter-bridge-for-br-medicaldevice.php
+++ br-costcenter-bridge-for-br-medicaldevice/module.br-costcenter-bridge-for-br-medicaldevice.php
@@ -3,14 +3,14 @@
 /**
  * @copyright   Copyright (C) 2024 Björn Rudner
  * @license     https://www.gnu.org/licenses/gpl-3.0.en.html
- * @version     2024-08-20
+ * @version     2024-09-10
  *
  * iTop module definition file
  */
 
 SetupWebPage::AddModule(
     __FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-    'br-costcenter-bridge-for-br-medicaldevice/0.4.0',
+    'br-costcenter-bridge-for-br-medicaldevice/2.7.4',
     array(
         // Identification
         'label' => 'Bridge - Costcenter + Medicaldevice',
@@ -18,8 +18,8 @@ SetupWebPage::AddModule(
 
         // Setup
         'dependencies' => array(
-            'br-costcenter/0.4.0',
-            'br-costcenter/0.4.0||br-medicaldevice/0.7.0',
+            'br-costcenter/2.7.4',
+            'br-costcenter/2.7.4||br-medicaldevice/2.7.9',
         ),
         'mandatory' => false,
         'visible' => true,
--- br-costcenter/module.br-costcenter.php
+++ br-costcenter/module.br-costcenter.php
@@ -3,14 +3,14 @@
 /**
  * @copyright   Copyright (C) 2024 Björn Rudner
  * @license     https://www.gnu.org/licenses/gpl-3.0.en.html
- * @version     2024-08-20
+ * @version     2024-09-10
  *
  * iTop module definition file
  */
 
 SetupWebPage::AddModule(
     __FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-    'br-costcenter/0.4.0',
+    'br-costcenter/2.7.4',
     array(
         // Identification
         //
