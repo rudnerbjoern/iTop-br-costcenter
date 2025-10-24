9c289c7f0c5dbb4cd27b4b1a6751e203e9fe1c42
Björn Rudner <rudnerbjoern@hotmail.com>
2025-10-24 14:12:21 +0200

    add costcenter to pmp-light

---
+++ br-costcenter-bridge-for-combodo-pmp-light/dictionaries/en.dict.br-costcenter-bridge-for-combodo-pmp-light.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @copyright   Copyright (C) 2024-2025 Björn Rudner
+ * @license     https://www.gnu.org/licenses/gpl-3.0.en.html
+ * @version     2025-10-23
+ *
+ * Localized data
+ */
+
+/** @disregard P1009 Undefined type Dict */
+Dict::Add('EN US', 'English', 'English', array(
+    'Class:Project/Attribute:costcenter_id' => 'Cost center',
+    'Class:Project/Attribute:costcenter_id+' => 'Cost center',
+    'Class:Project/Attribute:costcenter_name' => 'Cost center name',
+    'Class:Project/Attribute:costcenter_name+' => 'Cost center name',
+));
---
+++ br-costcenter-bridge-for-combodo-pmp-light/module.br-costcenter-bridge-for-combodo-pmp-light.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * @copyright   Copyright (C) 2024-2025 Björn Rudner
+ * @license     https://www.gnu.org/licenses/gpl-3.0.en.html
+ * @version     2025-10-23
+ *
+ * iTop module definition file
+ */
+
+/** @disregard P1009 Undefined type SetupWebPage */
+SetupWebPage::AddModule(
+    __FILE__, // Path to the current file, all other file names are relative to the directory containing this file
+    'br-costcenter-bridge-for-combodo-pmp-light/3.2.1',
+    array(
+        // Identification
+        'label' => 'Costcenter Bridge: PMP Light',
+        'category' => 'business',
+
+        // Setup
+        'dependencies' => array(
+            'itop-config-mgmt/3.2.0',
+            'combodo-pmp-light/1.1.0',
+            'br-costcenter/3.2.1',
+            'br-costcenter/3.2.1||combodo-pmp-light/1.1.0',
+        ),
+        'mandatory' => false,
+        'visible' => true,
+        'auto_select' => 'SetupInfo::ModuleIsSelected("br-costcenter") && SetupInfo::ModuleIsSelected("combodo-pmp-light")',
+
+        // Components
+        'datamodel' => array(),
+        'webservice' => array(),
+        'dictionary' => array(),
+        'data.struct' => array(),
+        'data.sample' => array(),
+
+        // Documentation
+        'doc.manual_setup' => '',
+        'doc.more_information' => '',
+
+        // Default settings
+        'settings' => array(),
+    )
+);
